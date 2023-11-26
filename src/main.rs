274c69d4b7edf1fbe0c5eae8c31a29bd1eb7fda0
Doct0rLekter <marikwinters@gmail.com>
2023-11-25 23:56:59 -0500

    Remove shell script and spawn camera

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new().add_plugins(DefaultPlugins).add_systems(Startup, setup).run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
